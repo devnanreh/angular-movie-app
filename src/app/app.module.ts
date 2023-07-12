797b4b12655243352002e203ac80a58933eb5f35
Hernan <hernan.albiol@gmail.com>
2023-07-12 11:56:27 +0200

    Add header with search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from "./app.component";
 import { MovieComponent } from "./components/movie/movie.component";
 import { MovieDetailsComponent } from "./components/movie-details/movie-details.component";
 import { HomeComponent } from "./components/home/home.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HomeComponent } from "./components/home/home.component";
     HomeComponent,
     MovieComponent,
     MovieDetailsComponent,
+    HeaderComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
