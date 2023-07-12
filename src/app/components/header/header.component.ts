fcffcbf53aec2347922ce9aac941e9d843b30bf2
Hernan <hernan.albiol@gmail.com>
2023-07-13 01:07:12 +0200

    Create reusable header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,23 +1,7 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { MoviesService } from "src/app/services/movies.service";
-
+import { Component } from "@angular/core";
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.css"],
 })
-export class HeaderComponent implements OnInit {
-  constructor(private movieService: MoviesService, private router: Router) {}
-
-  ngOnInit(): void {}
-
-  onSubmitSearchMovie(title: string) {
-    title = title.trim();
-
-    if (title.length === 0) {
-      return;
-    }
-    this.router.navigate(["/search", title]);
-  }
-}
+export class HeaderComponent {}
